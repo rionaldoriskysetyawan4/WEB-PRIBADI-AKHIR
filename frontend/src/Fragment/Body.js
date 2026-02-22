02ad082f6d9a4ae699e5b8f55ee82ab03bfc016d
rionaldoriskysetyawan4 <rionaldoriskysetyawan4@gmail.com>
2026-02-22 11:51:59 +0700

    Add dot patterns to Body component and create dot-container2 class for styling

--- frontend/src/Fragment/Body.js
+++ frontend/src/Fragment/Body.js
@@ -24,11 +24,27 @@ const Body = () => {
                 <div className="background-layer" style={{ top: '29%', width: '100%', height: '35%' }}></div>
 
                 <Vector />
-                {/* <div className="dot-pattern dot6" style={{ top: '2500px', left: '-8%' }}></div> */}
-                {/* <div className="dot-pattern dot7" style={{ top: '1900px', right: '-5%' }}></div> */}
             </div>
 
+            <div className="dot-container2">
+                <div className="dot-pattern dot1" style={{top: '5%', left: '-1.2%', transform: 'translate( -22%, -22%)'}}></div>
+                {/* <div className="dot-pattern dot2" style={{top: '13%', right: '5%',}}></div> */}
+                <div className="dot-pattern dot3" style={{top: '27.5%'}}></div>
+                <div className="dot-pattern dot4" style={{top: '34.5%', left: '9%'}}></div>
+                {/*  */}
+                <div className="dot-pattern dot9" style={{top: '8%', right: '35%'}}></div>
+                <div className="dot-pattern dot5" style={{top: '15%', left: '8%'}}></div>
+                <div className="dot-pattern dot8" style={{top: '18%', left: '35%'}}></div>
+                {/*  */}
+                <div className="dot-pattern dot6" style={{top: '40%', right: '-8%'}}></div>
+                <div className="dot-pattern dot5" style={{bottom: '34%'}}></div>
+                <div className="dot-pattern dot6" style={{bottom: '3%', left: '-8%'}}></div>
+                <div className="dot-pattern dot5" style={{bottom: '25%', right: '-8%'}}></div>
 
+                <div className="background-layer" style={{ top: '29%', width: '100%', height: '35%' }}></div>
+
+                <Vector />
+                </div>
         </div>
     );
 };
