00ba0f6ed97a4fcaddd21a4e4342f252fce8391e
rionaldoriskysetyawan4 <rionaldoriskysetyawan4@gmail.com>
2026-03-07 17:14:53 +0700

    Refactor dot patterns and background layers in Body component for improved styling and structure

--- frontend/src/Fragment/Body.js
+++ frontend/src/Fragment/Body.js
@@ -4,47 +4,29 @@ import Vector from '../Component/Vector';
 const Body = () => {
     return (
         <div className="body-wrapper">
-            <div className="dot-container1">
-                <div className="dot-pattern1 dot1" style={{top: '0%', left: '-1.2%', transform: 'translate( -22%, -22%)'}}></div>
-                <div className="dot-pattern1 dot2" style={{top: '13%', right: '5%',}}></div>
-                <div className="dot-pattern1 dot3" style={{top: '27.5%'}}></div>
-                <div className="dot-pattern1 dot4" style={{top: '34.5%', left: '9%'}}></div>
-                {/*  */}
-                <div className="dot-pattern1 dot9" style={{top: '8%', right: '35%'}}></div>
-                <div className="dot-pattern1 dot5" style={{top: '15%', left: '8%'}}></div>
-                <div className="dot-pattern1 dot8" style={{top: '18%', left: '35%'}}></div>
-                {/*  */}
-                <div className="dot-pattern1 dot6" style={{top: '40%', right: '-8%'}}></div>
-                <div className="dot-pattern1 dot5" style={{bottom: '34%'}}></div>
-                <div className="dot-pattern1 dot7" style={{bottom: '3%', left: '-8%'}}></div>
-                <div className="dot-pattern1 dot5" style={{bottom: '25%', right: '-8%'}}></div>
-                {/* Untuk test */}
-                {/* <div className="dot-pattern dot5" style={{bottom: '0%', right: '0%'}}></div> */}
 
-                <div className="background-layer1" style={{ top: '29%', width: '100%', height: '35%' }}></div>
+            <div className="dot-container">
+
+                <div className="dot-pattern dot1 pos1"></div>
+                <div className="dot-pattern dot2 pos2"></div>
+                <div className="dot-pattern dot3 pos3"></div>
+                <div className="dot-pattern dot4 pos4"></div>
+
+                <div className="dot-pattern dot9 pos5"></div>
+                <div className="dot-pattern dot5 pos6"></div>
+                <div className="dot-pattern dot8 pos7"></div>
+
+                <div className="dot-pattern dot6 pos8"></div>
+                <div className="dot-pattern dot5 pos9"></div>
+                <div className="dot-pattern dot7 pos10"></div>
+                <div className="dot-pattern dot5 pos11"></div>
+
+                <div className="background-layer"></div>
 
                 <Vector />
+
             </div>
 
-            <div className="dot-container2">
-                <div className="dot-pattern2 dot1" style={{top: '5%', left: '-1.2%', transform: 'translate( -22%, -22%)'}}></div>
-                <div className="dot-pattern2 dot2" style={{top: '13%', right: '5%',}}></div>
-                <div className="dot-pattern2 dot3" style={{top: '27.5%'}}></div>
-                <div className="dot-pattern2 dot4" style={{top: '34.5%', left: '9%'}}></div>
-                {/*  */}
-                <div className="dot-pattern2 dot9" style={{top: '8%', right: '35%'}}></div>
-                <div className="dot-pattern2 dot5" style={{top: '15%', left: '8%'}}></div>
-                <div className="dot-pattern2 dot8" style={{top: '18%', left: '35%'}}></div>
-                {/*  */}
-                <div className="dot-pattern2 dot6" style={{top: '40%', right: '-8%'}}></div>
-                <div className="dot-pattern2 dot5" style={{bottom: '34%'}}></div>
-                <div className="dot-pattern2 dot7" style={{bottom: '3%', left: '-8%'}}></div>
-                <div className="dot-pattern2 dot5" style={{bottom: '25%', right: '-8%'}}></div>
-
-                <div className="background-layer2" style={{ top: '5%', width: '100%', height: '10%' }}></div>
-
-                <Vector />
-                </div>
         </div>
     );
 };
